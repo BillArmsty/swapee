603ab901e9a1f88a371e6922bf21521ddd247def
BillArmsty <armsty9@gmail.com>
2022-12-15 15:13:20 +0300

    first commit

---
+++ dial.ts
@@ -0,0 +1,9 @@
+import { QDial, QDialSignals, QWidget } from '@nodegui/nodegui';
+
+class MyQDial extends QDial {
+  constructor(parent: QWidget) {
+    super(parent);
+
+    // Set any initial properties for the QDial here...
+  }
+}
