d2e5fc78b624df0b3f8f1ac868361eef473a3684
BillArmsty <armsty9@gmail.com>
2022-12-16 17:02:59 +0300

    Added trading view chart

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Chart from './components/chart'
+
+function App() {
+  return (
+    <h1>
+      Welcome to SWAPEE
+      <Chart />
+    </h1>
+  )
+}
+
+export default App
---
+++ src/components/chart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
+
+const Chart = () => (
+<AdvancedRealTimeChart theme="dark" autosize></AdvancedRealTimeChart>
+)
+export default Chart
